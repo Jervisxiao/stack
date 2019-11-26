34b093ff22a7dfd211ba010996b805df89aae94a
Jervisxiao <52239950+Jervisxiao@users.noreply.github.com>
2019-11-26 15:23:15 +0800

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,66 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_Empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(items)-1]
+    def size(self):
+        return len(self.items)
+
+
+    def parChecker(self,symbolString):
+        balanced = True
+        index = 0
+        while index < len(symbolString) and balanced:
+            symbol = symbolString[index]
+            if symbol in "({[":
+                self.push(symbol)
+            elif symbol in ")}]":
+                if self.is_Empty():
+                    balanced = False
+                else:
+                    self.pop()
+
+            index += 1
+        if balanced and self.is_Empty():
+            return True
+        else:
+            return False
+
+    def printText(self,symbolString):
+        if self.parChecker(symbolString) == True:
+            index = 0
+            text = ''
+            while index < len(symbolString): 
+                symbol = symbolString[index]
+                if symbol in "(":
+                    self.push(symbol)
+                elif symbol in ")":
+                    if self.is_Empty():
+                        balanced = False
+                    else:
+                        self.pop()
+                        if text != '':
+                            print(str(self.size()) + ': ' + text)
+                            text = ''                                              
+                else:
+                    text += symbolString[index]     
+                index += 1
+
+
+class myDict(dict):
+    def __init__(self, name, level, children):
+        self._dict = {'Name':name, 'level':level, 'children':children}
+
+    def add(id, val):
+        self._dict[id] = val
+
